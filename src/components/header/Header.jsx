711455ed20944bb5a787ff3f94362cee348730d0
JupDev <97832488+JupDiv@users.noreply.github.com>
2022-08-31 12:35:48 +0300

    add files and use my own styles in header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { months } from '../../utils/dateUtils';
+import moment from 'moment';
 
 import './header.scss';
 
 const Header = () => {
+  const [time, onUpdateTime] = useState(moment(new Date()).format('LTS'));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      onUpdateTime(moment(new Date()).format('LTS'));
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
-    <header className="header">
-      <button className="button create-event-btn">
-        <i className="fas fa-plus create-event-btn__icon"></i>Create
-      </button>
-      <div className="navigation">
-        <button className="navigation__today-btn button">Today</button>
-        <button className="icon-button navigation__nav-icon">
-          <i className="fas fa-chevron-left"></i>
+    <>
+      <header className="header">
+        <button className="button button_create-task create-event-btn">
+          <svg className="button_create-task-icon" width="28" height="28" viewBox="0 0 36 36">
+            <path fill="#34A853" d="M16 16v14h4V20z"></path>
+            <path fill="#4285F4" d="M30 16H20l-4 4h14z"></path>
+            <path fill="#FBBC05" d="M6 16v4h10l4-4z"></path>
+            <path fill="#EA4335" d="M20 16V6h-4v14z"></path>
+            <path fill="none" d="M0 0h36v36H0z"></path>
+          </svg>
+          <span className="button_create-task-text">Create</span>
         </button>
-        <button className="icon-button navigation__nav-icon">
-          <i className="fas fa-chevron-right"></i>
-        </button>
-        <span className="navigation__displayed-month"></span>
-      </div>
-    </header>
+        <div className="navigation">
+          <button className="navigation__today-btn button">Today</button>
+          <button className="icon-button navigation__nav-icon">
+            <i className="fas fa-chevron-left"></i>
+          </button>
+          <button className="icon-button navigation__nav-icon">
+            <i className="fas fa-chevron-right"></i>
+          </button>
+          <span className="navigation__displayed-month">month</span>
+        </div>
+        <div className="clock">{time}</div>
+      </header>
+      <hr className="header-line" />
+    </>
   );
 };
 
