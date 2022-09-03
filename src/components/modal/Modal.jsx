fe4771584e763ad3e587e1cfa8c309cda9fb8d12
JupDev <97832488+JupDiv@users.noreply.github.com>
2022-09-03 16:44:25 +0300

    fix a bit problem

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,8 +1,40 @@
 import React, { useState, useEffect } from 'react';
+import events from '../../gateway/events';
+import moment from 'moment';
 
 import './modal.scss';
 
 const Modal = ({ isClose }) => {
+  const [task, onCreateTask] = useState({
+    title: '',
+    description: '',
+    date: moment(new Date()).format('YYYY-MM-DD'),
+    startTime: moment().format('HH:mm'),
+    endTime: moment().format('HH:mm'),
+  });
+  const { title, description, date, startTime, endTime } = task;
+
+  const onHabdlerAddTask = e => {
+    const { name, value } = e.target;
+    onCreateTask({
+      ...task,
+      [name]: value,
+    });
+  };
+
+  const addHandlerTask = () => {
+    const createTask = {
+      id: events.length + 1,
+      title,
+      description,
+      dateFrom: new Date(`${date} ${startTime}`),
+      dateTo: new Date(`${date} ${endTime}`),
+    };
+
+    events.push(createTask);
+    isClose(false);
+  };
+
   return (
     <div className="modal overlay">
       <div className="modal__content">
@@ -10,25 +42,53 @@ const Modal = ({ isClose }) => {
           <button onClick={() => isClose(false)} className="create-event__close-btn">
             +
           </button>
-          <form className="event-form">
-            <input type="text" name="title" placeholder="Title" className="event-form__field" />
+          <form
+            onSubmit={e => {
+              e.preventDefault();
+              onHabdlerAddTask;
+            }}
+            className="event-form"
+          >
+            <input
+              onChange={onHabdlerAddTask}
+              type="text"
+              name="title"
+              placeholder="Title"
+              value={title}
+              className="event-form__field"
+            />
             <div className="event-form__time">
-              <input type="date" name="date" className="event-form__field" />
               <input
+                onChange={onHabdlerAddTask}
+                defaultValue={date}
+                type="date"
+                name="date"
+                className="event-form__field"
+              />
+              <input
+                onChange={onHabdlerAddTask}
                 type="time"
                 name="startTime"
                 className="event-form__field"
-                // onChange={() => onChange()}
+                defaultValue={startTime}
               />
               <span>-</span>
-              <input type="time" name="endTime" className="event-form__field" />
+              <input
+                onChange={onHabdlerAddTask}
+                defaultValue={endTime}
+                type="time"
+                name="endTime"
+                className="event-form__field"
+              />
             </div>
             <textarea
+              onChange={onHabdlerAddTask}
               name="description"
               placeholder="Description"
               className="event-form__field"
+              value={description}
             ></textarea>
-            <button type="submit" className="event-form__submit-btn">
+            <button onClick={addHandlerTask} type="button" className="event-form__submit-btn">
               Create
             </button>
           </form>
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 import './sidebar.scss';
 
-const Sidebar = (props) => {
+const Sidebar = () => {
   const hours = Array(24)
     .fill()
     .map((val, index) => index);
 
   return (
     <div className="calendar__time-scale">
-      {hours.map((hour) => (
-        <div className="time-slot">
+      {hours.map(hour => (
+        <div key={hour} className="time-slot">
           <span className="time-slot__time">{`${hour}:00`}</span>
         </div>
       ))}
